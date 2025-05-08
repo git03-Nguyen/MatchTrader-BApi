6935d1c3855202de8cdf388e854150ef3b6dd985
git03-Nguyen <nguyendinhanhvlqt@gmail.com>
2025-05-08 10:49:42 +0700

    Add gRPC endpoints setup

--- MatchTraderBApi/Options/MTrSettingsOptions.cs
+++ MatchTraderBApi/Options/MTrSettingsOptions.cs
@@ -2,7 +2,8 @@ namespace MatchTraderBApi.Options;
 
 public class MTrSettingsOptions
 {
-    public string Host { get; set; } = string.Empty;
+    public string RestHost { get; set; } = string.Empty;
+    public string gRpcHost { get; set; } = string.Empty;
     public string ApiKey { get; set; } = string.Empty;
     public string SystemUuid { get; set; } = string.Empty;
     public string ApiVersion { get; set; } = string.Empty;
