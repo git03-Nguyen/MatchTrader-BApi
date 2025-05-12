d7e82d391b61853f52c7d7f3fcff02409ee74a1d
Datqhz <hodat2215@gmail.com>
2025-05-12 13:55:28 +0700

    Add endpoint for accounts

--- MatchTraderBApi/Services/Interfaces/IMTrGeneralBrokerApi.cs
+++ MatchTraderBApi/Services/Interfaces/IMTrGeneralBrokerApi.cs
@@ -1,3 +1,6 @@
+using MatchTraderBApi.Enums;
+using MatchTraderBApi.Enums.SortingFields;
+using MatchTraderBApi.Models.Requests.General;
 using MatchTraderBApi.Models.Responses;
 using MatchTraderBApi.Models.Responses.General;
 
@@ -6,8 +9,8 @@ namespace MatchTraderBApi.Services.Interfaces;
 public interface IMTrGeneralBrokerApi
 {
     Task<MTrResponse<MTrGetServiceInfoResponse>> GetServiceInfo();
-    Task<MTrResponse<MTrGetBranchesResponse>> GetBranches();
-    Task<MTrResponse<MTrGetOffersResponse>> GetOffers();
-    Task<MTrResponse<MTrGetRolesResponse>> GetRoles();
-    Task<MTrResponse<MTrRetrievePlatformLogsV2Response>> RetrievePlatformLogsV2();
+    Task<MTrResponse<MTrGetBranchesResponse>> GetBranches(DateTime? from, DateTime? to, MTrBasicSortingField? sortField, MTrSortingOrder? sortOrder);
+    Task<MTrResponse<MTrGetOffersResponse>> GetOffers(DateTime? from, DateTime? to, MTrBasicSortingField? sortField, MTrSortingOrder? sortOrder);
+    Task<MTrResponse<MTrGetRolesResponse>> GetRoles(DateTime? from, DateTime? to, MTrBasicSortingField? sortField, MTrSortingOrder? sortOrder);
+    Task<MTrResponse<MTrRetrievePlatformLogsV2Response>> RetrievePlatformLogsV2(int? page, int? size, DateTime? from, DateTime? to, MTrSortingOrder? sortOrder, MTrRetrievePlatformLogsV2Request request);
 }
